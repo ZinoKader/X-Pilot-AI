c23bfa5b5526f2c2c4347406f4cf00305c34fabd
Zino Kader <zinka766@student.liu.se>
2016-10-10 17:24:16 +0200

    nytt namn på stub --> exc2, fungerar OK

---
+++ exc2.py
@@ -0,0 +1,113 @@
+#
+# This file can be used as a starting point for the bots.
+#
+
+import sys
+import traceback
+import math
+import libpyAI as ai
+from optparse import OptionParser
+
+#
+# Global variables that persist between ticks
+#
+tickCount = 0
+mode = "ready"
+# add more if needed
+
+def tick():
+    #
+    # The API won't print out exceptions, so we have to catch and print them ourselves.
+    #
+    try:
+        #
+        global tickCount
+        global mode
+
+        #
+        # Reset the state machine if we die.
+        #
+        if not ai.selfAlive():
+            tickCount = 0
+            mode = "ready"
+            return
+
+        tickCount += 1
+
+        #
+        # Read some "sensors" into local variables, to avoid excessive calls to the API
+        # and improve readability.
+        #
+
+        selfX = ai.selfX()
+        selfY = ai.selfY()
+        selfVelX = ai.selfVelX()
+        selfVelY = ai.selfVelY()
+        selfSpeed = ai.selfSpeed()
+
+        selfHeading = ai.selfHeadingRad()
+        # 0-2pi, 0 in x direction, positive toward y
+
+        # Add more sensors readings here
+
+        checkpoint = ai.nextCheckpoint()
+
+        checkpointX = ai.checkpointX(checkpoint) - selfX
+        checkpointY = ai.checkpointY(checkpoint) - selfY
+
+        targetDirection = math.atan2(checkpointY, checkpointX)
+
+        ai.turnToRad(targetDirection)
+
+        angledifference = angleDiff(selfHeading, targetDirection)
+
+
+        if angledifference <= 0.05:
+            mode = "travel"
+
+        print ("tick count:", tickCount, "mode", mode)
+
+
+        if mode == "ready":
+            pass
+
+        if mode == "travel":
+            ai.thrust()
+
+
+    except:
+        print(traceback.print_exc())
+
+
+
+def angleDiff(one, two):
+    """Calculates the smallest angle between two angles"""
+
+    a1 = (one - two) % (2*math.pi)
+    a2 = (two - one) % (2*math.pi)
+    return min(a1, a2)
+
+
+#
+# Parse the command line arguments
+#
+parser = OptionParser()
+
+parser.add_option ("-p", "--port", action="store", type="int",
+                   dest="port", default=15345,
+                   help="The port number. Used to avoid port collisions when"
+                   " connecting to the server.")
+
+(options, args) = parser.parse_args()
+
+name = "Stub"
+
+#
+# Start the AI
+#
+
+ai.start(tick,["-name", name,
+               "-join",
+               "-turnSpeed", "64",
+               "-turnResistance", "0",
+               "-port", str(options.port)])
