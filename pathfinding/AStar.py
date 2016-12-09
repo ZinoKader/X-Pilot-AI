5bf0a17d7339a53543965d9d8c43f59b04d3fe2c
Zino Kader <zinokad@gmail.com>
2016-12-09 10:47:23 +0100

    -

--- pathfinding/AStar.py
+++ pathfinding/AStar.py
@@ -1,71 +1,165 @@
-global grid
+"""
+The pathfinding module can be used to find the shortest
+path between two points in a graph.
 
-def gridDistance(startPos, endPos):
-    d = abs(startPos[0] - endPos[0]) + abs(startPos[1] - endPos[1])
-    return d
+To use the pathfinder for the default case:
 
-def getMin(lst, diction):
-    minNeighb = lst[0]
-    for i in lst:
-        if  diction[tuple(minNeighb)] > diction[tuple(i)]:
-            minNeighb = i
+>>> finder = pathfinder()
+>>> finder( (0,0), (1,1) )
+(2, [(0, 0), (0, 1), (1, 1)])
 
-    return minNeighb
+Or, to customize the pathfinder via passed in functions to handle for your
+particular graph implementation:
 
-def getNeighbors(position, grid):
-    look = [[0,1],[1,0],[-1,0],[0,-1]]
-    neighbList = []
-    for vect in look:
-        # sum is a tuple now
-        sum = position[0] + vect[0], position[1] + vect[1]
-        if grid[sum[0]][sum[1]] == 1:
-            neighbList.append(list(sum))
+>>> finder = pathfinder( distance=absolute_distance,        \\
+...                      cost=fixed_cost(2),                \\
+...                      neighbors=grid_neighbors(10,10) );
+>>> finder( (0,0), (2,2) )
+(8, [(0, 0), (0, 1), (1, 1), (1, 2), (2, 2)])
 
-    return neighbList
+If a maximum cost is specified, then an empty path will be returned if
+the cost exceeded the specified maximum
 
+>>> finder( (0,0), (2,2), 7 )
+(None, [])
+"""
 
-def astar(mapgrid, start, goal):
-    grid = mapgrid
-    closedSet = []
-    openSet = [start]
-    cameFrom = {}
-    gScore = {}
-    gScore[tuple(start)] = 0 # cost from start to start 0 of course
-    # estimated total cost from start to goal through y
-    fScore = {}
+import math
 
-    fScore[tuple(start)] = gScore[tuple(start)] + gridDistance(start, goal)
+def manhattan_distance( start, end ):
+    """
+    Calculate the manhattan distance between two points.
+    >>> manhattan_distance( (0,0), (5,5) )
+    10
+    """
+    return abs( start[ 0 ] - end[ 0 ] ) + abs( start[ 1 ] + end[ 1 ] )
 
-    # while openSet not empty...
-    while openSet:
-        current = getMin(openSet, fScore)
-        if current == goal:
-            return reconstructPath(cameFrom, goal)
+def absolute_distance( start, end ):
+    """
+    Calculate the distance between two points using the distance formula.
+    >>> absolute_distance( (1,2), (5,5) )
+    5.0
+    """
+    return math.sqrt( ( start[0] - end[0] )**2 + ( start[1] - end[1] )**2 )
 
-        openSet.remove(current)
-        closedSet.append(current)
-        for neighb in getNeighbors(current, grid) :
-            if neighb in closedSet:
-                continue
-            tentativeGScore = gScore[tuple(current)] + 1 # add the distance of the neighbor to the cost of the path
-            if neighb not in openSet:
-                openSet.append(neighb)
-            elif tentativeGScore >= gScore[tuple(neighb)]:
-                continue # this is not a better path
+def fixed_cost( cost ):
+    """
+    Return a fixed cost for all coordinates in the graph.
+    >>> cost = fixed_cost( 20 )
+    >>> cost( 1, 2 )
+    20
+    >>> cost( 3, 4 )
+    20
+    """
+    def func( a, b ):
+        return cost
+    return func
 
-            # the path is the best until now so record it
-            cameFrom[tuple(neighb)] = current
-            gScore[tuple(neighb)] = tentativeGScore
-            fScore[tuple(neighb)] = gScore[tuple(neighb)] + gridDistance(neighb, goal)
+def grid_neighbors( height, width ):
+    """
+    Calculate neighbors for a simple grid where
+    a movement can be made up, down, left, or right.
 
-    return False
+    Arguments:
+    height - The height of the grid
+    width - The width of the grid
 
+    >>> neighbor = grid_neighbors( 10, 10 )
+    >>> neighbor( (0,0) )
+    [(0, 1), (1, 0)]
+    >>> neighbor( (1,1) )
+    [(1, 2), (1, 0), (2, 1), (0, 1)]
 
-def reconstructPath(cameFrom, current):
-    totalPath = [current]
-    while tuple(current) in cameFrom.keys():
-        current = cameFrom[tuple(current)]
-        totalPath.append(current)
+    """
+    def func( coord ):
+        neighbor_list = [ ( coord[ 0 ], coord[ 1 ] + 1),
+                          ( coord[ 0 ], coord[ 1 ] - 1),
+                          ( coord[ 0 ] + 1, coord[ 1 ]),
+                          ( coord[ 0 ] - 1, coord[ 1 ])]
 
-    # get the reverse array...
-    return totalPath
+        return [ c for c in neighbor_list
+                 if c != coord
+                 and c[0] >= 0 and c[0] < width
+                 and c[1] >= 0 and c[1] < height ]
+    
+    return func
+
+def pathfinder( neighbors=grid_neighbors( 100, 100 ),
+                distance=absolute_distance,
+                cost=fixed_cost( 1 ) ):
+    """
+    Find the shortest distance between two nodes in a graph using the 
+    astar algorithm. By default, the graph is a coordinate plane where 
+    every node has the same cost and nodes can be traversed horizontally 
+    and vertically.
+
+    Keyword Arguments:
+    neighbor - Callable that takes a node and returns a list
+               of neighboring nodes.
+    distance - Callable that returns the estimated distance
+               between two nodes.
+    cost     - Callable that returns the cost to traverse
+               between two given nodes.
+    """
+    
+    def reconstruct_path( came_from, current_node ):
+        """Reconstruct the path from a given node to the beginning"""
+        if current_node in came_from:
+            p = reconstruct_path( came_from, came_from[ current_node ] )
+            p.append( current_node )
+            return p
+        else:
+            return [ current_node ]
+
+    def func( start, end, max_cost=None ):
+        """
+        Perform a-star pathfinding from a start to an
+        end coordinate.
+
+        Returns a tuple containing the cost associated with
+        the path, and a list of coordinates in the path
+
+        This implementation is based on the wikipedia pseudocode, which
+        translated almost directly into python.
+        http://en.wikipedia.org/wiki/A*_search_algorithm
+        """
+        open_set = set( [ start ] )
+        closed_set = set()
+        came_from = {}
+
+        g_score = { start : 0 }
+        f_score = { start : cost( start, end ) }
+
+        while len( open_set ) != 0:
+            current = min( open_set, key=lambda c: f_score[c] )
+
+            if max_cost != None and g_score[ current ] > max_cost:
+                break
+
+            if current == end:
+                return g_score[ current ], reconstruct_path( came_from, end )
+
+            open_set.discard( current )
+            closed_set.add( current )
+            for neighbor in neighbors( current ):
+                tentative_score = g_score[ current ] + cost( current, neighbor )
+
+                if neighbor in closed_set and ( neighbor in g_score and tentative_score >= g_score[ neighbor ] ):
+                    continue
+
+                if neighbor not in open_set or ( neighbor in g_score and tentative_score < g_score[ neighbor ] ):
+                    came_from[ neighbor ] = current
+                    g_score[ neighbor ] = tentative_score
+                    f_score[ neighbor ] = tentative_score + distance( neighbor, end )
+
+                    if neighbor not in open_set:
+                        open_set.add( neighbor )
+
+        return None, []
+    
+    return func
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
