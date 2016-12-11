0345e230b4b1af4bdc21f2846a385e2bc864739d
Zino Kader <zinokad@gmail.com>
2016-12-11 05:09:13 +0100

    -

--- pathfinding/implementation.py
+++ pathfinding/implementation.py
@@ -10,7 +10,7 @@ class SimpleGraph:
 
     def neighbors(self, id):
         return self.edges[id]
-        
+
 import collections
 
 class Queue:
@@ -72,6 +72,14 @@ class SquareGrid:
         results = filter(self.in_bounds, results)
         results = filter(self.passable, results)
         return results
+
+class GridWithWeights(SquareGrid):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.weights = {}
+
+    def cost(self, from_node, to_node):
+        return self.weights.get(to_node, 1)
 
 import heapq
 
--- pathfinding/maphelper.py
+++ pathfinding/maphelper.py
@@ -44,6 +44,7 @@ class MapHandler:
         """
         mapgrid = astar.SquareGrid(32, 32)
         mapgrid.walls = self.find_walls()
+        draw_grid(mapgrid)
         path = astar.a_star_search(mapgrid, start, goal)
         return path
 
