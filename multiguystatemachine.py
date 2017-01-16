d19bb9420f74a670b153231881b0564b67ed51dd
Zino Kader <zinokad@gmail.com>
2017-01-16 01:36:45 +0100

    -

--- multiguystatemachine.py
+++ multiguystatemachine.py
@@ -21,7 +21,7 @@ class MultiGuyStateMachine:
 
 
     def roam(self):
-        if self.states.is_ready() or self.states.is_roaming():
+        if self.states.is_ready() or self.states.is_roaming() or self.states.is_attacking_nearest():
             self.states.set_current_state("roaming")
             roamer = Roamer(self.ai, Navigator(self.ai, MapHandler(self.ai)))
             roamer.roam_random()
